3095cf8ee12a978b734cde2100b166d533070dd0
rmprieto <COLD_BITTER@movistar.es>
2024-08-21 19:01:44 +0200

    Create ejercicio3.js

---
+++ ejercicio3.js
@@ -0,0 +1,49 @@
+const users = [
+    {
+      name: "Alberto",
+      favoritesSounds: {
+        waves: { format: "mp3", volume: 50 },
+        rain: { format: "ogg", volume: 60 },
+        firecamp: { format: "mp3", volume: 80 },
+      },
+    },
+    {
+      name: "Antonio",
+      favoritesSounds: {
+        waves: { format: "mp3", volume: 30 },
+        shower: { format: "ogg", volume: 55 },
+        train: { format: "mp3", volume: 60 },
+      },
+    },
+    {
+      name: "Santiago",
+      favoritesSounds: {
+        shower: { format: "mp3", volume: 50 },
+        train: { format: "ogg", volume: 60 },
+        firecamp: { format: "mp3", volume: 80 },
+      },
+    },
+    {
+      name: "Laura",
+      favoritesSounds: {
+        waves: { format: "mp3", volume: 67 },
+        wind: { format: "ogg", volume: 35 },
+        firecamp: { format: "mp3", volume: 60 },
+      },
+    },
+  ];
+
+  const countFavoritesSounds = {};
+
+  for (const user of users){
+    const sounds = user.favoritesSounds;
+    for (const sound in sounds){
+        if(!countFavoritesSounds[sound]){
+            countFavoritesSounds[sound] = 1;
+        } else {
+            countFavoritesSounds[sound]++;
+        }
+     }
+ }  
+
+console.log(countFavoritesSounds);
