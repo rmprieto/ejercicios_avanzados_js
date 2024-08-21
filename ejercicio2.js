59dd6345b70983dd04d99630c748607f481cff30
rmprieto <COLD_BITTER@movistar.es>
2024-08-21 19:01:39 +0200

    Create ejercicio2.js

---
+++ ejercicio2.js
@@ -0,0 +1,50 @@
+const users = [
+    {
+      name: "Alberto",
+      favoritesSounds: {
+        waves: { format: "mp3", volume: 50 },
+        rain: { format: "ogg", volume: 60 },
+        firecamp: { format: "mp3", volume: 80 },
+      },
+    },
+    {
+      name: "Antonio",
+      favoritesSounds: {
+        waves: { format: "mp3", volume: 30 },
+        shower: { format: "ogg", volume: 55 },
+        train: { format: "mp3", volume: 60 },
+      },
+    },
+    {
+      name: "Santiago",
+      favoritesSounds: {
+        shower: { format: "mp3", volume: 50 },
+        train: { format: "ogg", volume: 60 },
+        firecamp: { format: "mp3", volume: 80 },
+      },
+    },
+    {
+      name: "Laura",
+      favoritesSounds: {
+        waves: { format: "mp3", volume: 67 },
+        wind: { format: "ogg", volume: 35 },
+        firecamp: { format: "mp3", volume: 60 },
+      },
+    },
+  ];
+
+let totVolume = 0;
+let contVolume = 0;
+
+for (const user of users){
+    //console.log(user.favoritesSounds);
+    const sound = user.favoritesSounds;
+    for (const key in sound){
+       totVolume += sound[key].volume;
+       contVolume++;
+    }
+}
+let medVolume = totVolume / contVolume;
+console.log('La media de los volumenes es:' + medVolume);
+
+
